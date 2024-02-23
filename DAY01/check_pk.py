58c34bc2386a9dea89fb62edc46f23417d0fa26a
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2024-02-23 14:44:59 +0900

    make readme

---
+++ DAY01/check_pk.py
@@ -0,0 +1,14 @@
+## 설치 패키지 버전 체크
+import pandas as pd
+import sklearn
+import matplotlib
+import seaborn
+import bs4
+import selenium
+
+print(f'{"Pandas":<17} version : {pd.__version__}')
+print(f'{"Scikit-learn":<17} version : {sklearn.__version__}')
+print(f'{"Matplotlib":<17} version : {matplotlib.__version__}')
+print(f'{"Seaborn":<17} version : {seaborn.__version__}')
+print(f'{"beautifulsoup":<17} version : {bs4.__version__}')
+print(f'{"selenium":<17} version : {selenium.__version__}')
